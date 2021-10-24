b1ec0531f5f66c15b9a32c2fa71b67d5c13f33a6
nijimajohn <86758487+nijimajohn@users.noreply.github.com>
2021-10-24 16:12:18 +0900

    Initial commit

---
+++ src/components/Child4.jsx
@@ -0,0 +1,13 @@
+const style = {
+  height: "200px",
+  backgroundColor: "wheat",
+  padding: "8px"
+};
+
+export const Child4 = () => {
+  return (
+    <div style={style}>
+      <p>Child4</p>
+    </div>
+  );
+};
